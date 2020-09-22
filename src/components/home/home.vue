6d486936b83f04741c4a7351017eea250fbf193d
杨国庆 <yangguoqing@SH-yanggq.local>
2020-09-22 10:42:06 +0800

    头部样式

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div>home
-  </div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+            <img src="../../assets/logo.png" alt="无法显示图片">
+            
+          </div>
+        </el-col>
+        <el-col :span="18" class="middle">
+          <h3>后台管理系统</h3>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple"><a class="loginout" href="">退出</a></div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
-  name:'home',
+  name: "home",
   components: {},
   props: {},
   data() {
-    return {
-    };
+    return {};
   },
   methods: {}
 };
 </script>
 <style scoped>
+.container {
+  height: 100%;
+}
+.header {
+  background-color: #b3c0d1;
+}
+.aside {
+  background-color: #d3dce6;
+}
+.main {
+  background-color: #e9eef3;
+  height: 100%;
+}
+.middle {
+  text-align: center;
+}
+.loginout{
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
